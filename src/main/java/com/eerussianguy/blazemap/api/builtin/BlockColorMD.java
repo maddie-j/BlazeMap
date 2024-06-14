683b9d15c21fe59a62d0e99ea024438ddd628971
maddie-j <no45483361@hotmail.com>
2024-06-14 23:21:40 +1000

    BME-131 [WIP] Attempting to put transparency on a separate layer

--- src/main/java/com/eerussianguy/blazemap/api/builtin/BlockColorMD.java
+++ src/main/java/com/eerussianguy/blazemap/api/builtin/BlockColorMD.java
@@ -2,21 +2,22 @@ package com.eerussianguy.blazemap.api.builtin;
 
 import java.util.Arrays;
 
-import com.eerussianguy.blazemap.api.BlazeMapReferences;
 import com.eerussianguy.blazemap.api.BlazeRegistry;
 import com.eerussianguy.blazemap.api.pipeline.DataType;
 import com.eerussianguy.blazemap.api.pipeline.MasterDatum;
 
 public class BlockColorMD extends MasterDatum {
+    private final BlazeRegistry.Key<DataType<MasterDatum>> id;
     public final int[][] colors;
 
-    public BlockColorMD(int[][] colors) {
+    public BlockColorMD(BlazeRegistry.Key<DataType<MasterDatum>> id, int[][] colors) {
+        this.id = id;
         this.colors = colors;
     }
 
     @Override
     public BlazeRegistry.Key<DataType<MasterDatum>> getID() {
-        return BlazeMapReferences.MasterData.BLOCK_COLOR;
+        return id;
     }
 
     @Override
