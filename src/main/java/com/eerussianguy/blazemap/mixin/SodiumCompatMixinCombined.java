b3a90c72c6b4ae2cf8869725939b3b5d75d6b012
maddie-j <no45483361@hotmail.com>
2025-05-07 21:57:36 +1000

    BME-195 Fixin' Mixins for Sodium to pass along Level data

---
+++ src/main/java/com/eerussianguy/blazemap/mixin/SodiumCompatMixinCombined.java
@@ -0,0 +1,52 @@
+package com.eerussianguy.blazemap.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.eerussianguy.blazemap.engine.MDSources;
+import com.eerussianguy.blazemap.engine.client.ClientEngine;
+import com.eerussianguy.blazemap.profiling.Profilers;
+
+import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
+import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderList;
+import me.jellysquid.mods.sodium.client.render.chunk.RenderSection;
+import net.minecraft.client.multiplayer.ClientLevel;
+
+public class SodiumCompatMixinCombined {
+    
+    @Mixin(value = SodiumWorldRenderer.class, remap = false)
+    public static interface SodiumWorldRenderedMixin {
+        @Accessor("world")
+        public ClientLevel getWorld();
+    }
+
+    
+    @Mixin(value = RenderSection.class, remap = false)
+    public static interface RenderSectionMixin {
+        @Accessor("worldRenderer")
+        public SodiumWorldRenderer getWorld();
+    }
+
+
+    @Mixin(value = ChunkRenderList.class, remap = false)
+    public static class ChunkRenderListMixin {
+
+        @Inject(method = "add", at = @At("HEAD"), remap = false)
+        void onAdd(RenderSection render, CallbackInfo ci) {
+            Profilers.Client.Mixin.SODIUM_LOAD_PROFILER.hit();
+            Profilers.Client.Mixin.SODIUM_TIME_PROFILER.begin();
+
+            ClientEngine.onChunkChanged(
+                ((SodiumWorldRenderedMixin)((RenderSectionMixin)render).getWorld()).getWorld().dimension(), 
+                render.getChunkPos().chunk(), 
+                MDSources.Client.SODIUM
+            );
+
+            Profilers.Client.Mixin.SODIUM_TIME_PROFILER.end();
+        }
+    }
+
+}
